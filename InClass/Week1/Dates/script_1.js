d8e3f82b7d6d3423860c587c063fd142ac94aadf
irene.delatorre <delatorr@mit.edu>
2018-02-07 22:53:07 -0500

    Created Date tutorial and exercise

---
+++ InClass/Week1/Dates/script_1.js
@@ -0,0 +1,25 @@
+//Select #date and write the current date
+// write it in an standard format
+
+
+
+
+
+
+
+// select #time and write the current time
+
+
+
+
+
+
+
+
+//select #year and write your birth year
+
+
+
+
+// select #difference and write how many days are left until your next birthday
+// round the number to remove the decimals
---
+++ InClass/Week1/Dates/solution/script_1.js
@@ -0,0 +1,42 @@
+//Select #date and write the current date
+
+var currentDate = new Date();
+
+// date in an standard format
+document.getElementById("date").innerHTML = currentDate.toDateString();
+
+
+// select #time and write the current time
+var hour = currentDate.getHours();
+var minutes = currentDate.getMinutes();
+var seconds = currentDate.getSeconds();
+
+var currentHour = "" + hour + ":" + minutes + ":" + seconds;
+
+document.getElementById("time").innerHTML = currentHour;
+
+
+//select #year and write your birth year
+var myBirthday = new Date("21 October 1987");
+
+var birthYear = myBirthday.getFullYear();
+
+document.getElementById("year").innerHTML = birthYear;
+
+
+
+// select #difference and write how many days are left until your next birthday
+// round the number to remove the decimals
+var birthdayThisYear = new Date ((myBirthday.getMonth()+1) + "/" + myBirthday.getDate() + "/" + currentDate.getFullYear());
+var difference = birthdayThisYear - currentDate;
+var differenceInDays = Math.floor(difference / (1000*3600*24));
+
+if (differenceInDays <0){
+    differenceInDays = -1 * differenceInDays;
+}
+
+// console.log(birthdayThisYear);
+//
+// console.log(differenceInDays);
+
+document.getElementById("difference").innerHTML = differenceInDays;
