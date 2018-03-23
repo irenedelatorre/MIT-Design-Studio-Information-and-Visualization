8cc767c21c6333da4c7832ffab44fd08d924abb5
irene.delatorre <delatorr@mit.edu>
2018-03-23 16:27:31 -0400

    Added exercise 6 solution

--- InClass/Week6/script_1.js
+++ InClass/Week6/script_1.js
@@ -12,28 +12,86 @@ var plot1 = d3.select('#plot1') // if we select a html id #name, if we select a 
 
 // function to draw the map
 
+var populationPerState = d3.map();
+
 // queue data files, parse them and use them
 var queue = d3.queue()
     .defer(d3.csv, "data/data.csv", parseData)
     .defer(d3.json, "data/us_map.json") //downloaded from https://d3js.org/us-10m.v1.json
+    .defer(d3.csv, "data/population.csv", parsePopulation) //downloaded from https://d3js.org/us-10m.v1.json
     .await(dataloaded);
 
 function dataloaded (err,data,map){
 
+    console.log(data)
+    console.log(map)
+    console.log(populationPerState)
+
     // get max and min values of data
+    var extentData = d3.extent(data,function(d){return d.total});
+    var extentPerCapitaData = d3.extent(data,function(d){
+        var stateID = d.id;
+        var statePopulation = populationPerState.get(stateID).estimate2017
+
+        return d.total/statePopulation});
+
+    // var colorScale = d3.scaleLinear().domain(extentData).range(["#ffc5c0","#ab0405"]);
+    var colorScale = d3.scaleLinear().domain(extentPerCapitaData).range(["#ffc5c0","#ab0405"]);
 
     // scale Color for the map
 
     // Bind the data to the SVG and create one path per GeoJSON feature
+    var path = d3.geoPath();
+
+    plot1.selectAll(".state")
+        .data(topojson.feature(map,map.objects.states).features)
+        .enter()
+        .append('path')
+        .attr("d",path)
+        .attr("class","state")
+        .style("fill",function(d){
+            var mapID = +d.id;
+            var color = "#000";
+            var totalPopulation = populationPerState.get(mapID).estimate2017;
+            
+            data.forEach(function(e){
+                if (e.id === mapID){
+                    color = colorScale(e.total/totalPopulation)
+                }
+            });
+            return color
+        })
 
 }
-
-
 
 // total: +d["Total; Estimate; Population 3 years and over enrolled in school"],
 //     percentage: +d["Percent; Estimate; Population 3 years and over enrolled in school"]
 
 
 function parseData(d){
+    var id = d.Id.split("US")[1];
 
+    return {
+        id: +id,
+        state: d.state,
+        total: +d["Total; Estimate; Population 3 years and over enrolled in school"]
+    }
+}
+
+
+
+function parsePopulation (d){
+    var id;
+
+    if (d.id !== ""){
+        id = +d.id.split("US")[1]
+    }else{
+        id = d["Geographic Area"]
+    }
+
+    populationPerState.set(id, {
+        state: d["Geographic Area"],
+        april2010: +d["April 1, 2010, Census"],
+        estimate2017: +d["Estimate 2017"]
+    })
 }
